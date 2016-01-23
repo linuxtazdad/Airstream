67facbe8f6c1866ac21522cfc77efa490cf1b706
Jeremy Levi <jeremy@acerlt.localdomain>
2016-01-22 20:22:18 -0800

    This is just the first commet to a new rep.. As of this time the Los Angeles script is working as I want but the OC script is not working the way I would like.

---
+++ readme.txt
@@ -0,0 +1,2 @@
+this is the directry I made for working on the script I am doing to get 
+the web invtory 
---
+++ script/Links-OC.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#this script pulles the invtory lins from Ebiz auto for a single store
+#below is where the url is set 
+# this could be a good scritp for pulling only a set kind of link off any web site list.
+URL='http://airstream-orange-county.ebizautos.com/inventory.aspx'
+EXTRA='_page=' 
+PAGE=1
+MORE_PAGE=2
+while [ $MORE_PAGE \> "1" ] ;do
+	sleep 	1
+	lynx -dump -source -accept_all_cookies  "$URL?$EXTRA$PAGE">dump.tmp 
+	echo Lynx 
+	grep detail dump.tmp |sed -ne 's/.*\(http[^"]*\).*/\1/p'|sed 's/..$//'>>links.txt 
+	#this command is not working the way it needs to look in to it latter 
+	let MORE_PAGE=$(grep -c next dump.tmp) 
+	let PAGE=PAGE+1
+	echo sleeping
+	sleep 30
+	#rm dump.tmp
+done
+
+wc -l links.txt
+
+
