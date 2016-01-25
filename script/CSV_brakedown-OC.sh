2b13c9ff7ab964f220dd9ef0200daa3c706b7d03
Jeremy Levi <jlevi79@gmail.com>
2016-01-24 20:28:23 -0800

    got the oc stock number working

--- script/CSV_brakedown-OC.sh
+++ script/CSV_brakedown-OC.sh
@@ -11,7 +11,7 @@ MODLE_TRIM=$(echo $i|sed 's/^.\{52\}//g'|sed 's/.\{14\}$//'|sed 's/\-/ /g'|sed '
 STOCK_TYPE=$(echo $i|sed 's/.\{14\}$//'|sed 's/.*-//g')
 lynx -dump -accept_all_cookies $i >Files\/$FILE_NAME
 #this group has to read the file not the "$i"
-STOCK_NUMBER=$(cat Files\/$FILE_NAME|grep -i stock|sed 's/.*: //g')
+STOCK_NUMBER=$(cat Files\/$FILE_NAME|grep -i stock|sed 's/.*: //g'|cut -d' ' -f1)
 VIN_NUMBER=$(cat Files\/$FILE_NAME|grep -i -A1 vin:|tail -n 1)
 INT_COLOR=$(cat Files\/$FILE_NAME|grep -i -A1 "int color"|tail -n 1)
 EXT_COLOR=$(cat Files\/$FILE_NAME|grep -i -A1 "color:"|tail -n 1)
@@ -23,5 +23,5 @@ echo $STOCK_NUMBER,$VIN_NUMBER,$MODLE_YEAR,$MODLE_MANF,$MODLE_TYPE,$MODLE_TRIM,$
 echo $STOCK_NUMBER,$VIN_NUMBER,$MODLE_YEAR,$MODLE_MANF,$MODLE_TYPE,$MODLE_TRIM,$STOCK_TYPE,$EXT_COLOR,$INT_COLOR,$PRICE_MARK,$PRICE_SHOW,
 echo sleeping
 sleep 20
-rm -f Files\/$FILE_NAME
+#rm -f Files\/$FILE_NAME
 done 
