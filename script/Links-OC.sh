f1646a969e0cdc42e2474c6cd8a015bed12a0d87
Jeremy Levi <jlevi79@gmail.com>
2016-02-02 06:19:19 -0800

    changing so it does not echo and having the OC script to its own temp files

--- script/Links-OC.sh
+++ script/Links-OC.sh
@@ -8,15 +8,15 @@ PAGE=1
 MORE_PAGE=2
 while [ $MORE_PAGE \> "1" ] ;do
 	sleep 	1
-	lynx -dump -source -accept_all_cookies  "$URL?$EXTRA$PAGE">dump.tmp 
-	echo Lynx 
-	grep detail dump.tmp |sed -ne 's/.*\(http[^"]*\).*/\1/p'|sed 's/..$//'>>links.txt 
-	#this command is not working the way it needs to look in to it latter 
+	lynx -dump -source -accept_all_cookies  "$URL?$EXTRA$PAGE">dump-oc.tmp 
+	#echo Lynx 
+	grep detail dump-oc.tmp |sed -ne 's/.*\(http[^"]*\).*/\1/p'|sed 's/..$//'>>links-oc.txt 
+	#this is the check to see if there is going to be a next page or not.. 
 	let MORE_PAGE=$(grep -c next dump.tmp) 
 	let PAGE=PAGE+1
-	echo sleeping
+	#echo sleeping
 	sleep 30
-	#rm dump.tmp
+	rm dump-oc.tmp
 done
 
 wc -l links.txt
--- script/Links.sh
+++ script/Links.sh
@@ -9,16 +9,16 @@ MORE_PAGE=2
 while [ $MORE_PAGE \> "1" ] ;do
 	sleep 	1
 	lynx -dump -source -accept_all_cookies  "$URL?$EXTRA$PAGE">dump.tmp 
-	echo Lynx 
+	#echo Lynx 
 	grep detail dump.tmp |sed -ne 's/.*\(http[^"]*\).*/\1/p'|sed 's/..$//'>>links.txt 
 	#this command is not working the way it needs to look in to it latter 
 	let MORE_PAGE=$(grep -c next dump.tmp) 
 	let PAGE=PAGE+1
-	echo sleeping
+	#echo sleeping
 	sleep 30
 	#rm dump.tmp
 done
 
-wc -l links.txt
+#wc -l links.txt
 
 
