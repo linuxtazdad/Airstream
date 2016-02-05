60ac9c5118c18e32911c5b789c97c43971919818
Jeremy Levi <jlevi79@gmail.com>
2016-02-04 17:40:29 -0800

    got the prosses worked out of how I am going to be getting the difrets and set the compare.sh script done and just need to work out bugs

---
+++ script/compare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#This file is to use the list from ADP with the web site.
+#this space is for cleaning the list from ADP
+#this is pulling the stock numbers from both files
+cat invtory.csv|cut -d"," -f1>>stock_number.tmp
+cat web_invtory.csv|cut -d"," -f1>>stock_number.tmp
+sort stock_number.tmp|uniq -u>dif_stock.tmp
+
+
+#this is where it starts comparring 
+for i in $(cat dif_stock.tmp)
+	do
+	grep $i web_invtory.csv>>dif_stock.csv
+	grep $i invtory.csv>> dif_stock.csv
+	done
+#rm stock_number.tmp 
+#rm dif_stock.tmp
