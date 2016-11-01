da6419774959cd38996f673ee0962e6bcb200ebe
Jeremy Levi <jlevi79@gmail.com>
2016-11-01 23:30:29 +0000

    just getting a clean working screen

--- script/compleat.sh
+++ script/compleat.sh
@@ -1,13 +1,17 @@
-!#/bin/bash
-//this is just a easy way to get it all taken care of.
-
-Links.sh
-
+#//this is just a easy way to get it all taken care of.
+alias dropbox="/home/jeremy/.dropscrit/dropbox_uploader.sh"
+echo "running  Links.sh"
 ./Links.sh
-dropbox download invtory/invtory.csv
+echo "getting invtory from dropbox"
+/home/jeremy/.dropscrit/./dropbox_uploader.sh download invtory/invtory.csv
+echo "comparing the files"
 ./compare.sh
-dropbox upload web_invtory.csv invtory/web_invtory.csv
-dropbox upload dif_stock.csv invtory/ dif_stock.csv
-rm dif_stock
-rm dif_stock.csv
-rm invtory.csv
+echo "uploading web invtory to dropbox"
+/home/jeremy/.dropscrit/./dropbox_upload.sh upload web_invtory.csv invtory/web_invtory.csv
+echo "uploading dif stock to dropbox"
+/home/jeremy/.dropscrit/./dropbox_upload.sh upload dif_stock.csv invtory/ dif_stock.csv
+echo "would be removing files" 
+#rm dif_stock
+#rm dif_stock.csv
+#rm invtory.csv
+#alias dropbox="/home/jeremy/.dropscrit/dropbox_uploader.sh"
