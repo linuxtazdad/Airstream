5569754e79a59160e3e10bb7efb032b4560cf761
Jeremy Levi <jlevi79@gmail.com>
2018-03-24 07:18:42 -0700

    fixing the change from CDK

--- script/compare.sh
+++ script/compare.sh
@@ -2,7 +2,7 @@
 #This file is to use the list from ADP with the web site.
 #this space is for cleaning the list from ADP
 #this is pulling the stock numbers from both files
-cat invtory.csv|cut -d"," -f13>>vin_number.tmp
+cat invtory.csv|cut -d"," -f17>>vin_number.tmp
 cat web_invtory.csv|cut -d"," -f6|sed 's/\"//g'>>vin_number.tmp
 sort vin_number.tmp|uniq -u>dif_vin.tmp
 #removing after dealer spike change over no longer needed
