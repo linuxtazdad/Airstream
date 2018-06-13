a4a6054631e2d986bab3fb023b2d8cecb8501589
linuxtazdad <jlevi79@gmail.com>
2018-06-13 06:41:07 -0700

    cleaning up the script to be quite

--- script/full_run.sh
+++ script/full_run.sh
@@ -4,17 +4,17 @@ now=$(date +"%m_%d_%Y")
 #no longer needed do to move to dealerspike website 02/2/18
 #echo "running  Links.sh"
 #./Links.sh
-echo "pulling web invtory from dealerspike"
+#echo "pulling web invtory from dealerspike"
 wget -q -O web_invtory.csv "www.airstreamlosangeles.com/feeds.asp?feed=GenericCSVFeed"
-echo "getting invtory from dropbox"
+#echo "getting invtory from dropbox"
 /home/jeremy/.dropscrit/./dropbox_uploader.sh download invtory/invtory.csv
-echo "comparing the files"
+#echo "comparing the files"
 ./compare.sh
 #echo "uploading web invtory to dropbox"
 /home/jeremy/.dropscrit/./dropbox_uploader.sh upload web_invtory.csv invtory/web_invtory.csv
-echo "uploading dif stock to dropbox"
+#echo "uploading dif stock to dropbox"
 /home/jeremy/.dropscrit/./dropbox_uploader.sh upload dif_stock.csv invtory/dif_stock.csv
-echo "would be removing files" 
+#echo "would be removing files" 
 rm dif_stock
 rm dif_stock.csv
 rm invtory.csv
