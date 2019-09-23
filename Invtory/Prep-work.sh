e899a5627b50f350a26ff52394c8d212a0568535
Jeremy Levi <jlevi79@gmail.com>
2019-09-23 06:37:22 -0700

    more work

--- Invtory/Prep-work.sh
+++ Invtory/Prep-work.sh
@@ -4,3 +4,8 @@
 ### Working forders listed below
 ### /CDK-Raw
 ### Web-Raw
+dropbox="/home/jeremy/.dropscrit/dropbox_uploader.sh"
+
+##pulling all the working files in to the directroy
+$dropbox download invtory/CDK-Raw  working/
+$dropbox download invtory/Web-site working/
--- Invtory/Web-Invtory.sh
+++ Invtory/Web-Invtory.sh
@@ -2,18 +2,20 @@
 ##This file is to pull and put the web_invtory in dropbox for the other scripts
 ##to uses it as needed.
 
+dropbox="/home/jeremy/.dropscrit/dropbox_uploader.sh"
 
 ###ALA web site pull
 wget -q -O ALA-web_invtory.csv "www.airstreamlosangeles.com/feeds.asp?feed=GenericCSVFeed"
-dropbox upload ALA-web_invtory.csv Invtory/Web-site/
+#/home/jeremy/.dropscrit/./dropbox_uploader.sh upload ALA-web_invtory.csv Invtory/Web-site/
+$dropbox upload ALA-web_invtory.csv Invtory/Web-site/
 ###AOC Web site pull
 wget -q -O AOC-web_invtory.csv "www.airstreamlosangeles.com/feeds.asp?feed=GenericCSVFeed"
-dropbox upload AOC-web_invtory.csv Invtory/Web-site/
+$dropbox upload AOC-web_invtory.csv Invtory/Web-site/
 ###AIE web site pull
 wget -q -O AIE-web_invtory.csv "www.airstreamlosangeles.com/feeds.asp?feed=GenericCSVFeed"
-dropbox upload AIE-web_invtory.csv Invtory/Web-site/
+$dropbox upload AIE-web_invtory.csv Invtory/Web-site/
 ###ALV web site pull
 wget -q -O ALV-web_invtory.csv "www.airstreamlosangeles.com/feeds.asp?feed=GenericCSVFeed"
-dropbox upload ALV-web_invtory.csv Invtory/Web-site/
+$dropbox upload ALV-web_invtory.csv Invtory/Web-site/
 ###Cleaning up Files
 rm *-web_invtory.csv
