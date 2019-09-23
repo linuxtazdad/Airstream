d651c8fae0d399120be181481c3824a6c788ff9c
Jeremy Levi <jlevi79@gmail.com>
2019-09-23 05:02:23 -0700

    getting everything ready to start the script for Invtory for all the stores

---
+++ Invtory/Prep-work.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+###This file is only going to be for getting all the files downloaded and ready
+###for the rest of the scripts to work.
+### Working forders listed below
+### /CDK-Raw
+### Web-Raw
---
+++ buying_script/working/CR-pricing.py
@@ -0,0 +1,2 @@
+#!/bin/python3
+#This is part of my buying script to pull year make model and price from craigslist
