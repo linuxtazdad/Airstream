fe00bf16043a9de018d820f04061a202fc204340
Jeremy Levi <jlevi79@gmail.com>
2020-02-13 09:13:10 -0800

    changing the way the prices show in the csv file.

--- Invtory/Bilding-Masterlist.sh
+++ Invtory/Bilding-Masterlist.sh
@@ -28,10 +28,10 @@ for i in $(cat Working/CDK-All-Store.csv) ;do
   MODEL_NUMBER=$(echo $i |cut -d ',' -f6|tr -d '[:space:]')
   OUTSIDE_COLOR=$(echo $i |cut -d ',' -f9|tr -d '[:space:]')
   INSIDE_COLOR=$(echo $i |cut -d ',' -f10|tr -d '[:space:]')
-  SALE_PRICE=$(echo $i |cut -d ',' -f12|tr -d '[:space:]')
-  MSRP=$(echo $i |cut -d ',' -f30|tr -d '[:space:]')
-  INVOICE=$(echo $i |cut -d ',' -f13|tr -d '[:space:]')
-  BALANCE=$(echo $i |cut -d ',' -f14|tr -d '[:space:]')
+  SALE_PRICE=$(echo $i |cut -d ',' -f12|tr -d '[:space:]'|xargs printf "$%.0f"|sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta')
+  MSRP=$(echo $i |cut -d ',' -f30|tr -d '[:space:]'|xargs printf "$%.0f"|sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta')
+  INVOICE=$(echo $i |cut -d ',' -f13|tr -d '[:space:]'|xargs printf "$%.0f"|sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta')
+  BALANCE=$(echo $i |cut -d ',' -f14|tr -d '[:space:]'|xargs printf "$%.0f"|sed -e :a -e 's/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/;ta')
   COMPANY_NUMBER=$(echo $i |cut -d ',' -f16|tr -d '[:space:]')
   AGE=$(echo $i |cut -d ',' -f17|tr -d '[:space:]')
   DEALER_NOTES=$(echo $i |cut -d ',' -f22|tr -d '[:space:]')
@@ -52,25 +52,25 @@ for i in $(cat Working/CDK-All-Store.csv) ;do
       ;;
   esac
 #Managers invtory
-echo $STOCK_NUMBER,$STOCK_TYPE,$MODEL_YEAR,$MAKE,$MODEL,$MODEL_NUMBER,$OUTSIDE_COLOR,$INSIDE_COLOR,$AGE,$MSRP,$SALE_PRICE,$BALANCE,$INVOICE,$LOCATED,$VIN_NUMBER,$DEALER_NOTES,>>Working/Managers-Invtory.tmp
+echo $STOCK_NUMBER,$STOCK_TYPE,$MODEL_YEAR,$MAKE,$MODEL,$MODEL_NUMBER,$OUTSIDE_COLOR,$INSIDE_COLOR,$AGE,\"$MSRP\",\"$SALE_PRICE\",\"$BALANCE\",\"$INVOICE\",$LOCATED,$VIN_NUMBER,$DEALER_NOTES,>>Working/Managers-Invtory.tmp
 #Sales person invotry
-echo $STOCK_NUMBER,$STOCK_TYPE,$MODEL_YEAR,$MAKE,$MODEL,$MODEL_NUMBER,$OUTSIDE_COLOR,$INSIDE_COLOR,$AGE,$MSRP,$SALE_PRICE,$LOCATED,$VIN_NUMBER,$DEALER_NOTES,>>Working/Sales-All-Invtory.tmp
+echo $STOCK_NUMBER,$STOCK_TYPE,$MODEL_YEAR,$MAKE,$MODEL,$MODEL_NUMBER,$OUTSIDE_COLOR,$INSIDE_COLOR,$AGE,\"$MSRP\",\"$SALE_PRICE\",$LOCATED,$VIN_NUMBER,$DEALER_NOTES,>>Working/Sales-All-Invtory.tmp
 #Physical invtory
 echo $STOCK_NUMBER,$STOCK_TYPE,$MODEL_YEAR,$MAKE,$MODEL,,$LOCATED,$VIN_NUMBER,$DEALER_NOTES,>>Working/Physical-All-Invtory.tmp
 done
 #Managers Invtory
 echo "stock No,Type,Year,Make,Model,Model#,ExtColor,IntColor,Age,MSRP,SalePrice,Balance,Invoice,LOT,VIN,Notes," >Working/Managers-Invtory.csv
 cat Working/Managers-Invtory.tmp >>Working/Managers-Invtory.csv
-$dropbox upload Working/Managers-Invtory.csv Invtory/Managers-Invtory.csv
+#$dropbox upload Working/Managers-Invtory.csv Invtory/Managers-Invtory.csv
 #Sales Person Invtory
 echo "stock No,Type,Year,Make,Model,Model#,ExtColor,IntColor,Age,MSRP,SalePrice,LOT,VIN,Notes," >Working/Sales-All-Invtory.csv
 grep -F -eNEW -eUSED Working/Sales-All-Invtory.tmp >>Working/Sales-All-Invtory.csv
 #cat Working/Sales-All-Invtory.tmp >>Working/Sales-All-Invtory.csv
-$dropbox upload Working/Sales-All-Invtory.csv Invtory/Sales-All-Invtory.csv
+#$dropbox upload Working/Sales-All-Invtory.csv Invtory/Sales-All-Invtory.csv
 #Physical invtory for alias
 echo "stock No,Type,Year,Make,Model,Model#,LOT,Store,VIN,Notes," >Working/Sales-All-Invtory.csv
 grep -F -eNEW -eUSED Working/Physical-All-Invtory.tmp >>Working/Physical-All-Invtory.csv
-$dropbox upload Working/Physical-All-Invtory.csv Invtory/Physical-All-Invtory.csv
+#$dropbox upload Working/Physical-All-Invtory.csv Invtory/Physical-All-Invtory.csv
 ##clean up mess at when all done.
-rm Working/*.csv
+#rm Working/*.csv
 rm Working/*.tmp
